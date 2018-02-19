ac050aa0c5eb737c919552c785261cf21cbeeb6d
Eva Monsen <eva.monsen@gmail.com>
2018-02-19 13:48:58 -0800

    Add requirements to setup.py packaging

--- setup.py
+++ setup.py
@@ -2,5 +2,9 @@ from setuptools import setup, find_packages
 setup(
     name = "regender",
     version = "0.0.1",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=[
+        "inflect",
+        "pattern",
+    ]
     )
